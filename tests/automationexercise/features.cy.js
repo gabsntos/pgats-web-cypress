2736431cfcb78aa55e7fba9818d428dc68995449
gabriel <gabriel545.545@gmail.com>
2025-11-01 00:35:35 -0300

    CI pipeline creation and some tweaks

--- tests/automationexercise/features.cy.js
+++ tests/automationexercise/features.cy.js
@@ -137,3 +137,20 @@ describe('Home page', () => {
     })
 
 })
+
+describe('Contact us page', () => {
+
+    beforeEach(() => {
+        menuPage.selectMenuOption('Contact us')
+    });
+
+    it('file uploading', () => {
+        cy.get('[data-qa="name"]').type(randomData().name)
+        cy.get('[data-qa="email"]').type(randomData().email)
+        cy.get('[data-qa="subject"]').type('file uploading test')
+        cy.get('textarea#message').type('help me')
+        cy.get('[name="upload_file"]').selectFile('./tests/fixture/fileUploadingTestFile.txt')
+        cy.get('[data-qa="submit-button"]').click()
+        cy.get('.contact-form div.status').should('have.text','Success! Your details have been submitted successfully.')
+    });
+});
